66d293f453583235f0db8656ee7463042585237d
igorrzinho <82171880+igorrzinho@users.noreply.github.com>
2023-04-13 03:25:41 -0300

    3 files modified, 1 added, 2 deleted

--- index.js
+++ index.js
@@ -1,35 +1,17 @@
 import express from 'express';
 import router from './route/router.js';
 import swaggerUi from 'swagger-ui-express';
-import swaggerJsDoc from 'swagger-jsdoc';
-import bodyParser from 'body-parser';
+import yaml from 'yamljs';
 import { createTableUsuarios } from './controller/usuarios.js';
 import { createTableTodo } from './controller/tarefas.js';
 const app = express();
 const port = 3000;
 app.use(express.json());
 
+const swaggerDocument = yaml.load('./openapi.yaml');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 createTableUsuarios();
 createTableTodo();
-
-const swaggerOptions = {
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'Minha API',
-      version: '1.0.0',
-      description: 'Descrição da minha API',
-    },
-    servers: [
-      {
-        url: 'http://localhost:3000',
-      },
-    ],
-  },
-  apis: ['./route/*.js'],
-};
-const swaggerDocs = swaggerJsDoc(swaggerOptions);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use('/', router);
 
