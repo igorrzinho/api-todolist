a6c996b8e16d610facc740df828c929a2c0af2a3
igor Silva <82171880+igorrzinho@users.noreply.github.com>
2023-05-12 00:52:41 -0300

    Add files via upload

--- route/router.js
+++ route/router.js
@@ -6,7 +6,7 @@ import {
   Login,
   Token,
   tokenValid,
-} from '../controller/usuarios.js';
+} from "../controller/usuarios.js";
 
 import {
   insertTask,
@@ -14,41 +14,41 @@ import {
   deleteAllTask,
   selectTask,
   allTasks,
-  updateTask
-} from '../controller/tarefas.js';
-import { auth } from './auth.js';
-import { Router } from 'express';
-import { randomUUID } from 'crypto';
+  updateTask,
+} from "../controller/tarefas.js";
+import { auth } from "./auth.js";
+import { Router } from "express";
+import { randomUUID } from "crypto";
 
 const router = Router();
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   res.json({
     usuarios: {
-      'adicionar usuario': '/adduser',
+      "adicionar usuario": "/adduser",
       body: {
-        email: 'string',
-        senha: 'password',
-        nomeUsuario: 'string',
+        email: "string",
+        senha: "password",
+        nomeUsuario: "string",
       },
-      'alterar um usuario': '/putuser/:nomeUsuario/:senha/:id',
-      'deletar um usuario': '/delete/:id',
-      'mostra todos os usuarios': '/allusers',
+      "alterar um usuario": "/putuser/:nomeUsuario/:senha/:id",
+      "deletar um usuario": "/delete/:id",
+      "mostra todos os usuarios": "/allusers",
     },
     tasks: {
-      'adiciona um nova task': '/addtask/:id',
-      body: { task: 'ola mundo' },
-      'seleciona as task pelo id do usuario': '/selecttask/:id',
-      'deleta uma task pelo id': '/deletetask/:id',
-      'mostra todas as tasks': '/alltasks',
+      "adiciona um nova task": "/addtask/:id",
+      body: { task: "ola mundo" },
+      "seleciona as task pelo id do usuario": "/selecttask/:id",
+      "deleta uma task pelo id": "/deletetask/:id",
+      "mostra todas as tasks": "/alltasks",
     },
   });
 });
 
 async function Authentication(req, res, next) {
-  const token = req.headers['token'];
+  const token = req.headers["token"];
   if (!token) {
-    return res.status(401).json({ mensagem: 'Token não encontrado' });
+    return res.status(401).json({ mensagem: "Token não encontrado" });
   }
 
   let resToken = await tokenValid(token);
@@ -56,7 +56,7 @@ async function Authentication(req, res, next) {
   // Aqui você pode verificar se o token é válido, por exemplo
 
   if (resToken.length < 1) {
-    return res.status(401).json({ mensagem: 'Token não é valido' });
+    return res.status(401).json({ mensagem: "Token não é valido" });
   }
 
   // Se o token for inválido, você pode retornar um erro 401 (Não Autorizado)
@@ -65,73 +65,72 @@ async function Authentication(req, res, next) {
 }
 //usuario----------------------------------------------------------------------------------------------------------------------------------------------------//
 
-router.post('/adduser', async function (req, res) {
+router.post("/adduser", async function (req, res) {
   let { email, senha, nomeUsuario } = req.body;
-  var tolken = randomUUID();
-  insertUser(email, senha, nomeUsuario, tolken);
+  insertUser(email, senha, nomeUsuario, randomUUID());
   let token = await Token(nomeUsuario, senha);
+  console.log(token);
   res.json(token);
 });
 
-router.post('/login', async (req, res) => {
+router.post("/login", async (req, res) => {
   const { nomeUsuario, senha } = req.body;
-  let dados = await Login(nomeUsuario, senha);
   let token = await Token(nomeUsuario, senha);
   let hasToken = auth(token);
   if (hasToken == true) {
     res.status(200).json(token);
   } else {
-    res.status(401).send('nao autorizado');
+    res.status(401).send("nao autorizado");
   }
 });
 
-router.put('/putuser/:id', Authentication, async function (req, res) {
+router.put("/putuser/:id", Authentication, async function (req, res) {
   let { id } = req.params;
   let { senha, nomeUsuario } = req.body;
   updatePessoa(nomeUsuario, senha, id);
-  res.json('usuario alterado');
+  res.json("usuario alterado");
 });
 
-router.delete('/delete/:id', async function (req, res) {
+router.delete("/delete/:id", async function (req, res) {
   let { id } = req.params;
   deleteUser(id);
   deleteAllTask(id);
-  res.json('usuario deletado');
+  res.json("usuario deletado");
 });
 
-router.get('/allusers', async function (req, res) {
+router.get("/allusers", async function (req, res) {
   let users = await allUsers();
   res.json(users);
 });
 
 //task----------------------------------------------------------------------------------------------------------------------------------------------------//
-router.post('/addtask/:id', Authentication, async function (req, res) {
+router.post("/addtask/:id", Authentication, async function (req, res) {
   let id = req.params.id;
   let task = req.body.task;
   insertTask(task, id);
-  res.json('tarefa adicionada: ' + task);
+  res.json("tarefa adicionada: " + task);
 });
 
-router.get('/selecttask/:id', Authentication, async function (req, res) {
+router.get("/selecttask/:id", Authentication, async function (req, res) {
   let id = req.params.id;
   let tasks = await selectTask(id);
   res.json(tasks);
 });
 
-router.delete('/deletetask/:id', Authentication, async function (req, res) {
+router.delete("/deletetask/:id", Authentication, async function (req, res) {
   let id = req.params.id;
   deleteTask(id);
-  res.json('tarefa deletada de id ' + id);
+  res.json("tarefa deletada de id " + id);
 });
 
-router.put('/puttask/:id', async function (req, res) {
-  let {complete} = req.body
+router.put("/puttask/:id", async function (req, res) {
+  let { complete } = req.body;
   let id = req.params.id;
-  updateTask(complete,id)
-  res.json('tarefa alterada de id ' + id);
+  updateTask(complete, id);
+  res.json("tarefa alterada de id " + id);
 });
 
-router.get('/alltasks', async function (req, res) {
+router.get("/alltasks", async function (req, res) {
   let tasks = await allTasks();
   res.json(tasks);
 });
